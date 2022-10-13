b012836e46b8e531d8217a4341cf646950a67e6e
PavanHathwar <pavan.hathwar@thoughtworks.com>
2022-10-13 18:45:33 +0530

    [pavan] used react-form-hook for adding validation

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,33 +1,45 @@
-import React , {useState} from 'react';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
-function Signin()
-{
-    const [name,setName]=useState("");
-    const [password,setPassword]=useState("");
-    return(
-        <div className="Signin">
-      
-        <form>
-            <div className='Name-container'>
-                <label>name:</label>
-                <input type="text" name="name" onChange={(e)=>setName(e.target.value)}></input>
-                
-            </div>
-            <div className='Password-container'>
-                <label>password:</label>
-                <input type="password" name="password" onChange={(e)=>setName(e.target.value)}></input>
-               
-            </div>
-            <div className='Submit-container'>
-                <input type="submit" value="submit"></input>
-            </div>
-            
-        </form>
-        
-            <Link to="/signup">Sign-Up?</Link>
-        
+function Signin() {
+  const { register, handleSubmit, formState:{errors} } = useForm();
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="Signin">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="Name-container">
+          <label>name:</label>
+          <input
+            type="text"
+            name="name"
+            {...register("name", { required: "enter your name" })}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+        </div>
+        <p>{errors?.name &&errors.name.message}</p>
+        <div className="Password-container">
+          <label>password:</label>
+          <input
+            type="password"
+            name="password"
+            {...register("password", { required: "enter the password" })}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+        </div>
+        <p>{errors?.password &&errors.password.message}</p>
+        <div className="Submit-container">
+          <input type="submit" value="submit"></input>
+        </div>
+      </form>
+
+      <Link to="/signup">Sign-Up?</Link>
     </div>
-    );
+  );
 }
 export default Signin;
